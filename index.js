95d29ce09587ecb6ca8fbed1aa4f9853c1410af9
khushboo2803 <khushboogrover.mca19.du@gmail.com>
2020-04-24 17:30:11 +0530

    updated

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express();
 const mongoose = require('mongoose')
 const db = process.env.DB;
+const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 
 require('./models/user');
@@ -21,7 +22,6 @@ app.get('/signup', (req,res)=>{
     res.send("signUp successful")
 })
 
-
-app.listen('5000', () => {
+app.listen(port, () => {
     console.log('running on 300');
 })
