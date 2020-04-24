f45b1c3eea9d08d5fdf84f6d17941e3643a3f19b
khushboo2803 <khushboogrover.mca19.du@gmail.com>
2020-04-24 17:17:32 +0530

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose')
-const db = process.env.db;
+const db = process.env.DB;
 const bodyParser = require('body-parser');
 
 require('./models/user');
