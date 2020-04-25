3aa2f02a91a845a606c0967d949b05c2747029a8
khushboo2803 <khushboogrover.mca19.du@gmail.com>
2020-04-25 11:14:11 +0530

    final

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const cors=require('cors');
 
-require('./models/user');
+const User=require('./models/user');
 app.use(cors())
 
 app.use(function(req,res,next){
