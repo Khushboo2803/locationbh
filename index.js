3d7325897d188211e282b35a40737dc644d3da98
khushboo2803 <khushboogrover.mca19.du@gmail.com>
2020-04-25 11:16:51 +0530

    final

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const cors=require('cors');
 
-const User=require('./models/user');
+const User=mongoose.model('user');;
 app.use(cors())
 
 app.use(function(req,res,next){
