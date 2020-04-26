a6fb47b09eba5062e16ccf0c31b605e9ffdcfac2
khushboo2803 <khushboogrover.mca19.du@gmail.com>
2020-04-26 13:01:21 +0530

    ok

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const mongoose = require('mongoose')
 const db = process.env.DB;
 const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
-//const cors=require('cors');
+const cors=require('cors');
 
 require('./models/user');
-// app.use(cors())
+app.use(cors())
 
-// app.use(function(req,res,next){
-//     res.header("Access-Control-Allow-Origin","*");
-//     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With,Content-Type,Accept");
-//     next();
-// });
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+   res.header("Access-Control-Allow-Headers","Origin, X-Requested-With,Content-Type,Accept");
+    next();
+ });
 
 const authRoutes=require('./routes/authRoutes')
 app.use(bodyParser.json());
